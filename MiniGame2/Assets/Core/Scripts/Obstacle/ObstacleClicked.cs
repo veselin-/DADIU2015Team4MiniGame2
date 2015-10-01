9eeac4ee21cd3a4fd14af6beea4125a74c5d2463
Bo Olsen <boolsen14@gmail.com>
2015-10-01 15:55:08 +0200

    You can now click on an obstacle to fly into it.

---
+++ MiniGame2/Assets/Core/Scripts/Obstacle/ObstacleClicked.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleClicked : MonoBehaviour {
+
+
+    private PlayerBoost playerBoost;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+        playerBoost = GameObject.FindGameObjectWithTag("Player").transform.parent.GetComponent<PlayerBoost>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnMouseDown()
+    {
+        playerBoost.MoveTowardsObstacle(this.transform.position, Input.mousePosition);
+           
+    }
+}
