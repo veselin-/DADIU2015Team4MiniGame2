ed8c156147df97ee7f0c24d22404c41e06305c38
Bo Olsen <boolsen14@gmail.com>
2015-10-01 16:15:02 +0200

    Changed the way the distance is being measured.

--- MiniGame2/Assets/Core/Scripts/Player/PlayerBoost.cs
+++ MiniGame2/Assets/Core/Scripts/Player/PlayerBoost.cs
@@ -38,7 +38,8 @@ public class PlayerBoost : MonoBehaviour
         //We use the y coordinate of the mouse vs the player because the obstacle always is in front of the player.
         float disPlayerAndMouseClick = Mathf.Abs(mousePosClicked.y - Camera.main.WorldToScreenPoint(transform.position).y);
 
-        if (!moveTowardsObject && disPlayerAndMouseClick > MinDistanceToObstacle && disPlayerAndMouseClick < MaxDistanceToObstacle)
+        //We need to make sure that the distance is taken care of in both the screen and world distance.
+        if (!moveTowardsObject && disPlayerAndMouseClick > MinDistanceToObstacle && disPlayerAndMouseClick < MaxDistanceToObstacle && Vector3.Distance(position, transform.position) < MaxDistanceToObstacle)
         {
             moveTowardsObject = true;
             targetPosition = new Vector3(position.x, this.transform.position.y, position.z);
