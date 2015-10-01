7145f293686afd28a8d1795f8eb34f4c5945da7b
Tomas Moesgaard <tomahawk@os.dk>
2015-10-01 11:13:41 +0200

    Elephant with particles added as prefab

---
+++ MiniGame2/Assets/ElephantParticleController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElephantParticleController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+
+        if (col.tag == "PickUp")
+        {
+            GetComponentInChildren<ParticleSystem>().Play();
+
+        }
+
+     
+
+    }
+}
