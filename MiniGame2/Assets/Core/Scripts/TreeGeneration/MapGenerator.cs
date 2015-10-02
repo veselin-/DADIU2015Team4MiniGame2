c69b2ad9689555f63e738dff33158cd57d87c442
Peter Troelsen <prtroelsen@gmail.com>
2015-10-02 10:14:56 +0200

    Finalized the tree generation, and removed old implementation

--- MiniGame2/Assets/Core/Scripts/TreeGeneration/MapGenerator.cs
+++ MiniGame2/Assets/Core/Scripts/TreeGeneration/MapGenerator.cs
@@ -9,13 +9,10 @@ namespace Assets.Core.Scripts.TreeGeneration
     {
         public int Width;
         public int Height;
+        public int NumberOfClosters;
 
         public string Seed;
         public bool UseRandomSeed;
-        public bool UserClusterGeneration;
-
-        [Range(0, 100)]
-        public int RandomFillPercent;
 
         [Range(0,100)]
         public int HeightUpperLimit ;
@@ -23,17 +20,17 @@ namespace Assets.Core.Scripts.TreeGeneration
         [Range(0, 100)]
         public int HeightLowerLimit;
 
-        public int NumberOfClosters;
+        [Range(0,100)]
+        public float SizeVariation;
+
+        public GameObject[] Trees;
 
         private List<List<Vector3>> _pointsInPlane;
         private float _largestY;
         private float _smallestY;
         private float _lowerBoundaryY;
         private float _upperBoundaryY;
-
-        public GameObject[] Trees;
-
-        int[,] map;
+        private int[,] _map;
 
         void Start()
         {
@@ -44,15 +41,18 @@ namespace Assets.Core.Scripts.TreeGeneration
             }
             Random.seed = Seed.GetHashCode();
 
+            // Get and order vertices
             var vertices = GetComponent<MeshFilter>().mesh.vertices;
             vertices = vertices.OrderBy(s => s.x).ThenBy(s => s.z).ToArray();
             var v1 = vertices.OrderBy(s => s.y);
 
+            // Lower and upper boundary
             _largestY = v1.LastOrDefault().y;
             _smallestY = v1.FirstOrDefault().y;
             _lowerBoundaryY = _smallestY + (_largestY - _smallestY) * (0.01f*HeightLowerLimit);
             _upperBoundaryY = _smallestY + (_largestY - _smallestY) * (0.01f*HeightUpperLimit);
 
+            // Create matrix with vertives
             _pointsInPlane = new List<List<Vector3>>();
             for (int i = 0; i < Width; i++)
             {
@@ -65,42 +65,32 @@ namespace Assets.Core.Scripts.TreeGeneration
                 _pointsInPlane.Add(t);
             }
 
-            if (UserClusterGeneration)
-            {
-                GenerateClusters();
-            }
-            else
-            {
-                GenerateMap();
-            }
-            
+            GenerateClusters();
             Draw();
         }
 
-        void Update()
-        {
-        }
-
-
         void GenerateClusters()
         {
-            map = new int[Width, Height];
+            _map = new int[Width, Height];
             var numCLusters = 0;
             var maxTries = 0;
             var r = new System.Random();
-            while (numCLusters < NumberOfClosters && maxTries++ < 500)
+
+            //Find some starting points, within the defines bounds (maxTries there to make sure no infinite loop.
+            while (numCLusters < NumberOfClosters && maxTries++ < 500 && _lowerBoundaryY < _upperBoundaryY)
             {
                 var x = r.Next(0, _pointsInPlane.Count);
                 var y = r.Next(0, _pointsInPlane[x].Count);
 
-                if (_pointsInPlane[x][y].y < _upperBoundaryY && _pointsInPlane[x][y].y > _lowerBoundaryY)
-                {
-                    map[x, y] = 1;
-                    numCLusters++;
-                }
-                Debug.Log(maxTries);
+                // Not a valid candidate
+                if (!(_pointsInPlane[x][y].y < _upperBoundaryY) || !(_pointsInPlane[x][y].y > _lowerBoundaryY))
+                    continue;
+
+                _map[x, y] = 1;
+                numCLusters++;
             }
 
+            // Actually adds trees to the cluster, otherwise only lonely trees
             for (var i = 0; i < 2; i++)
             {
                 SmoothClusters();
@@ -115,52 +105,12 @@ namespace Assets.Core.Scripts.TreeGeneration
                 {
                     int neighbourWallTiles = GetSurroundingWallCount(x, y);
 
-                    if (map[x, y] == 1)
+                    if (_map[x, y] == 1)
                         continue;
                     if (neighbourWallTiles > 0 && Random.Range(0,100) < 15)
-                        map[x, y] = 1;
+                        _map[x, y] = 1;
                     else
-                        map[x, y] = 0;
-                }
-            }
-        }
-
-        void GenerateMap()
-        {
-            map = new int[Width, Height];
-            RandomFillMap();
-
-            for (int i = 0; i < 1; i++)
-            {
-                SmoothMap();
-            }
-        }
-
-
-        void RandomFillMap()
-        {
-            for (int x = 0; x < Width; x++)
-            {
-                for (int y = 0; y < Height; y++)
-                {
-                    map[x, y] = (Random.Range(0, 100) < RandomFillPercent) ? 1 : 0;
-                }
-            }
-        }
-
-        void SmoothMap()
-        {
-            for (int x = 0; x < Width; x++)
-            {
-                for (int y = 0; y < Height; y++)
-                {
-                    int neighbourWallTiles = GetSurroundingWallCount(x, y);
-
-                    if (neighbourWallTiles > 4)
-                        map[x, y] = 1;
-                    else if (neighbourWallTiles < 4)
-                        map[x, y] = 0;
-
+                        _map[x, y] = 0;
                 }
             }
         }
@@ -176,7 +126,7 @@ namespace Assets.Core.Scripts.TreeGeneration
                     {
                         if (neighbourX != gridX || neighbourY != gridY)
                         {
-                            wallCount += map[neighbourX, neighbourY];
+                            wallCount += _map[neighbourX, neighbourY];
                         }
                     }
                 }
@@ -184,16 +134,15 @@ namespace Assets.Core.Scripts.TreeGeneration
 
             return wallCount;
         }
-
-
+        
         void OnDrawGizmos()
         {
-            if (map == null) return;
+            if (_map == null) return;
 
             for (var x = 0; x < Width; x++)
                 for (var y = 0; y < Height; y++)
                 {
-                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+                    Gizmos.color = (_map[x, y] == 1) ? Color.black : Color.white;
                     var pos = new Vector3(-Width / 2 + x + .5f, 0, -Height / 2 + y + .5f);
                     Gizmos.DrawCube(pos, Vector3.one);
                 }
@@ -202,13 +151,13 @@ namespace Assets.Core.Scripts.TreeGeneration
         private void Draw()
         {
             var thisMatrix = transform.localToWorldMatrix;
-            if (map == null) return;
+            if (_map == null) return;
 
             for (int x = 0; x < Width; x++)
                 for (int y = 0; y < Height; y++)
                 {
                     // There should not be a tree here
-                    if (map[x,y] == 0)
+                    if (_map[x,y] == 0)
                         continue;
 
                     // Are we outsite the boundary?
@@ -217,7 +166,17 @@ namespace Assets.Core.Scripts.TreeGeneration
                         continue;
 
                     // Spawn the tree at that position
-                    Instantiate(Trees[Random.Range(0, Trees.Length)], thisMatrix.MultiplyPoint3x4(pointInPlain), Quaternion.identity);
+                    var tree = (GameObject)Instantiate(Trees[Random.Range(0, Trees.Length)], thisMatrix.MultiplyPoint3x4(pointInPlain), Quaternion.identity);
+
+                    // Random rotation
+                    var rot = tree.transform.localRotation;
+                    rot.y = Random.Range(0, 360);
+                    tree.transform.rotation = rot;
+
+                    // Random scale
+                    var scale = tree.transform.localScale;
+                    scale = scale*(1+Random.Range(0, SizeVariation) /100f);
+                    tree.transform.localScale = scale;
                 }
         }
 
