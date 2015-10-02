9e3fc6e2c862370f2ee284ddf5d444b73c2e7e6c
Tomas Moesgaard <tomahawk@os.dk>
2015-10-02 13:43:49 +0200

    Added tutorial tools

---
+++ MiniGame2/Assets/TutorialTriggerControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TutorialTriggerControl : MonoBehaviour
+{
+
+    public Text TextBox;
+
+    public string TutorialText;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	    TextBox.enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+
+            Time.timeScale = 0f;
+
+            TextBox.text = TutorialText;
+
+            TextBox.enabled = true;
+
+
+        }        
+    }
+
+}
