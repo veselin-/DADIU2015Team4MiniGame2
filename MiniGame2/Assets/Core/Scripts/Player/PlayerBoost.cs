04d896b45eaa454393d10a9357e8acfa349de533
Nicholas <king-devlin@hotmail.com>
2015-10-05 15:49:55 +0200

    Beta Build

--- MiniGame2/Assets/Core/Scripts/Player/PlayerBoost.cs
+++ MiniGame2/Assets/Core/Scripts/Player/PlayerBoost.cs
@@ -60,19 +60,34 @@ public class PlayerBoost : MonoBehaviour
     public void MoveTowardsObstacle(Vector3 position)
     {
         // We need some amount of adrenaline to do this
-        if (adrenalineController.AdrenalineBar.value < MinAdrenalin)
-            return;
-
-        //The cosine of the angle between the two vectors, it will be positive if the target position is in front of the player
-        Vector3 heading = position - transform.position;
-        float dot = Vector3.Dot(heading, transform.forward);
-
-        if (dot < MaxDistanceToObstacle && dot > MinDistanceToObstacle)
+        if (adrenalineController.AdrenalineBar.value >= MinAdrenalin)
         {
-            moveTowardsObject = true;
-            targetPosition = new Vector3(position.x, position.y, position.z);
-            adrenalineController.DecreaseAdrenaline(25f);
+            //The cosine of the angle between the two vectors, it will be positive if the target position is in front of the player
+            Vector3 heading = position - transform.position;
+            float dot = Vector3.Dot(heading, transform.forward);
+
+            if (dot < MaxDistanceToObstacle && dot > MinDistanceToObstacle)
+            {
+                moveTowardsObject = true;
+                targetPosition = new Vector3(position.x, position.y, position.z);
+                adrenalineController.DecreaseAdrenaline(25f);
+            }
         }
+    }
+
+
+    public void FixCollider1(float i)
+    {
+        StartCoroutine(FixCollider2(i));
+
+    }
+
+    public IEnumerator FixCollider2(float HitSafePeriod)
+    {
+        this.gameObject.GetComponent<Collider>().enabled = false;
+        yield return new WaitForSeconds(HitSafePeriod);
+        this.gameObject.GetComponent<Collider>().enabled = true;
+
     }
 }
 
