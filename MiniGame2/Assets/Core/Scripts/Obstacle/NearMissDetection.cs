d67e3276de7ae8b0722aeacdb5ec097b381da23e
Peter Troelsen <prtroelsen@gmail.com>
2015-10-06 13:30:18 +0200

    Increased time before near miss is applied

--- MiniGame2/Assets/Core/Scripts/Obstacle/NearMissDetection.cs
+++ MiniGame2/Assets/Core/Scripts/Obstacle/NearMissDetection.cs
@@ -23,7 +23,7 @@ public class NearMissDetection : MonoBehaviour {
 
     IEnumerator NearMiss(GameObject go)
     {
-        yield return new WaitForSeconds(.1f);
+        yield return new WaitForSeconds(.5f);
         if (go.GetComponent<Collider>().enabled)
       {
             adrenalineController.IncreaseAdrenaline(AdrenalineAwarded);
