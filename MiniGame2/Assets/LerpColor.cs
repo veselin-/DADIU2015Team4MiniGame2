7104ead8ac6e3423e89b9fdabdecccf4dba656f4
Peter Troelsen <prtroelsen@gmail.com>
2015-10-06 17:27:44 +0200

    Made it so the tree stump is not flashing, when not enough adrenalin

--- MiniGame2/Assets/LerpColor.cs
+++ MiniGame2/Assets/LerpColor.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LerpColor : MonoBehaviour {
 
@@ -9,19 +10,31 @@ public class LerpColor : MonoBehaviour {
     private bool fromWhiteToRed = true;
     public float ColorSpeed = 5f;
 
-     private float t = 0; // lerp control variable
-
+    private float t = 0; // lerp control variable
+    float _minAdrenalin;
+    private Slider adrenalineSlider;
 
     // Use this for initialization
     void Start()
     {
         rend = GetComponent<Renderer>();
         rend.material.shader = Shader.Find("Standard");
+
+        _minAdrenalin = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBoost>().MinAdrenalin;
+        adrenalineSlider = GameObject.FindGameObjectWithTag("UI").GetComponent<AdrenalineController>().AdrenalineBar;
+
     }
 
     void Update()
     {
+        // If not white, and not enough adrenalin, turn white
+        if (adrenalineSlider.value < _minAdrenalin && rend.material.color != Color.white)
+            rend.material.color = Color.white;
 
+        // If not enough adrenalin, don't flash
+        if (adrenalineSlider.value < _minAdrenalin)
+            return;
+        
         if (t < 1)
         { // while t below the end limit...
           // increment it at the desired rate every update:
